6b4fa498873aeb21860298686141585be0dcd151
mukundagrawal <49935539+mukundagrawal@users.noreply.github.com>
2020-04-10 19:03:45 +0530

    Update Main.java

--- Problems/Decrement all/src/Main.java
+++ Problems/Decrement all/src/Main.java
@@ -2,19 +2,13 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-       /* Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         // put your code here
         int a=scanner.nextInt();
         int b=scanner.nextInt();
         int c=scanner.nextInt();
         int d=scanner.nextInt();
         System.out.print((a-1)+" "+(b-1)+" "+(c-1)+" "+(d-1));
-    }
-
-        */
-        String str = " Inside Main";
-        String subs = str.substring(0, 6);
-        long n = subs.trim().length();
-        System.out.println(n);
+    
     }
 }
